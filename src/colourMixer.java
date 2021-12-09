81dadd655cacdd641920dcba04797370a96ea0cb
Manuel Notter <notter.manuel@gmail.com>
2021-12-09 19:05:18 +0100

    added bracket and file

---
+++ src/colourMixer.java
@@ -0,0 +1,19 @@
+
+public class colourMixer {
+
+	public enum Colour {
+		BLUE, YELLOW, RED, LILAC, GREEN, ORANGE
+	}
+	private Colour colourFinish, colour;
+	
+	
+	//sets Colours
+	public void setColour(Colour colour) {
+		this.colourFinish = colourFinish;
+	}
+	
+	// gets Colours
+	public Colour getColour (){
+		return colour;
+	}
+}
