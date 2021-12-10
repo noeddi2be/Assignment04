b9f4e099fc0100d15b50574f44b3df38b5aff0c4
Manuel Notter <notter.manuel@gmail.com>
2021-12-10 09:25:37 +0100

    almost finished

--- src/Colourmaker.java
+++ src/Colourmaker.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Colourmaker implements ColourShuffle {
+public class Colourmaker {
 
 	public enum Colour {
 		BLUE, YELLOW, RED, LILA, GREEN, ORANGE
@@ -34,34 +34,5 @@ public class Colourmaker implements ColourShuffle {
 		}
 
 		return finColour;
-	}
-	
-
-	public Colour shuffle() {
-		Random ran2 = new Random();
-		int set = ran2.nextInt(6)+1;
-		Colour colour1 = Colour.BLUE;
-		Colour colour2 = Colour.YELLOW;
-		Colour colour3 = Colour.RED;
-		Colour colour4 = Colour.LILA;
-		Colour colour5 = Colour.GREEN;
-		Colour colour6 = Colour.ORANGE;
-		Colour finColour;
-
-		if (set == 1) {
-			finColour = colour1;
-		} else if (set == 2) {
-			finColour = colour2;
-		} else if (set == 3) {
-			finColour = colour3;
-		} else if (set == 4) {
-			finColour = colour4;
-		} else if (set == 5) {
-			finColour = colour5;
-		} else {
-			finColour = colour6;
-		}
-
-		return finColour;
-	}
+	}	
 }
--- src/RandomObject.java
+++ src/RandomObject.java
@@ -1,9 +1,39 @@
-public class RandomObject {
+import java.util.Random;
+
+
+public class RandomObject implements ColourShuffle {
 
     private int randomUnevenNumber;
     private String randomAnimal;
     private Colourmaker.Colour randomColour;
 
+    public Colourmaker.Colour shuffle() {
+		Random ran2 = new Random();
+		int set = ran2.nextInt(6)+1;
+		Colourmaker.Colour colour1 = Colourmaker.Colour.BLUE;
+		Colourmaker.Colour colour2 = Colourmaker.Colour.YELLOW;
+		Colourmaker.Colour colour3 = Colourmaker.Colour.RED;
+		Colourmaker.Colour colour4 = Colourmaker.Colour.LILA;
+		Colourmaker.Colour colour5 = Colourmaker.Colour.GREEN;
+		Colourmaker.Colour colour6 = Colourmaker.Colour.ORANGE;
+		Colourmaker.Colour finColour;
+
+		if (set == 1) {
+			finColour = colour1;
+		} else if (set == 2) {
+			finColour = colour2;
+		} else if (set == 3) {
+			finColour = colour3;
+		} else if (set == 4) {
+			finColour = colour4;
+		} else if (set == 5) {
+			finColour = colour5;
+		} else {
+			finColour = colour6;
+		}
+
+		return finColour;
+    }
 
     public void setRandomUnevenNumber(int number) {
         this.randomUnevenNumber = number;
